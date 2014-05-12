032ae3d9d7355f2d5e96dbd05e11a34c61776efd
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2014-05-12 00:52:50 -0600

    ENH: Add Date Checker utility

---
+++ ITTDMMAWHTTTF/YVBDateChecker.h
@@ -0,0 +1,19 @@
+//
+//  YVBDateChecker.h
+//  ITTDMMAWHTTTF
+//
+//  Created by Yoshiki Vázquez Baeza on 5/12/14.
+//  Copyright (c) 2014 Yoshiki Development Team. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface YVBDateChecker : NSObject{
+	BOOL result;
+}
+
+@property (atomic, readonly) BOOL result;
+-(BOOL)getResult;
+
+@end
---
+++ ITTDMMAWHTTTF/YVBDateChecker.m
@@ -0,0 +1,52 @@
+//
+//  YVBDateChecker.m
+//  ITTDMMAWHTTTF
+//
+//  Created by Yoshiki Vazquez Baeza on 5/12/14.
+//  Copyright (c) 2014 Yoshiki Development Team. All rights reserved.
+//
+
+#import "YVBDateChecker.h"
+
+
+@implementation YVBDateChecker
+
+@synthesize result=_result;
+
+-(id)init{
+	if (self = [super init]) {
+		_result=NO;
+	}
+	return self;
+}
+
+-(BOOL)_checkDateForDate:(NSDate *)date{
+	NSDate *theDate;
+	NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+	[formatter setTimeStyle:NSDateFormatterNoStyle];
+	[formatter setDateFormat:@"MM/dd/yyyy"];
+
+	// http://www.october212015.com
+	// http://istodaythedaymartymcflyarriveswhenhetravelstothefuture.com
+	theDate = [formatter dateFromString:@"10/25/2015"];
+
+	if ([theDate compare:date] == NSOrderedSame){
+		return YES;
+	}
+
+	return NO;
+}
+
+-(BOOL)getResult{
+	NSDateFormatter *formatter=[[NSDateFormatter alloc] init];
+	[formatter setDateFormat:@"MM/dd/yyyy"];
+	NSDate *today;
+	today=[formatter dateFromString:[formatter stringFromDate:[NSDate date]]];
+
+	_result = [self _checkDateForDate:today];
+
+	return _result;
+}
+
+
+@end
